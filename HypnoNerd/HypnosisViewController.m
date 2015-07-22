3565c8efc38bf3630f078dbbef9281209e752457
CoreyGoodie <coreygoodermote@gmail.com>
2015-07-21 22:43:38 -0400

    Added ui label shit

--- HypnoNerd/HypnosisViewController.m
+++ HypnoNerd/HypnosisViewController.m
@@ -9,6 +9,10 @@
 #import "HypnosisViewController.h"
 #import "HypnosisView.h"
 
+@interface HypnosisViewController () <UITextFieldDelegate>
+
+@end
+
 @implementation HypnosisViewController
 
 - (void)loadView
@@ -16,6 +20,15 @@
     //Create a view
     HypnosisView *backgroundView = [[HypnosisView alloc] init];
     //Set it as *the* view of the controller
+    CGRect textFieldRect = CGRectMake(40, 70, 240, 30);
+    UITextField *textField = [[UITextField alloc] initWithFrame:textFieldRect];
+    
+    // Setting the border style on the text field will allow us to see it more easily
+    textField.borderStyle = UITextBorderStyleRoundedRect;
+    [backgroundView addSubview:textField];
+    textField.placeholder = @"Hypnotize me";
+    textField.returnKeyType = UIReturnKeyDone;
+    textField.delegate = self;
     self.view = backgroundView; 
 }
 
@@ -31,4 +44,39 @@
     return self;
 }
 
+- (BOOL)textFieldShouldReturn:(UITextField *)textField{
+    //NSLog(@"%@",textField.text);
+    [self drawHypnoticMessage:textField.text];
+    textField.text = @"";
+    [textField resignFirstResponder];
+    return YES;
+}
+
+- (void)drawHypnoticMessage:(NSString *)message
+{
+    for (int i = 0; i < 20; i++){
+        UILabel *messageLabel = [[UILabel alloc]init];
+        messageLabel.backgroundColor = [UIColor clearColor];
+        messageLabel.textColor = [UIColor redColor];
+        messageLabel.text = message;
+        
+        //Make text label relative
+        [messageLabel sizeToFit];
+        
+        int width = (int)(self.view.bounds.size.width - messageLabel.bounds.size.width);
+        int x = arc4random() % width;
+        
+        // Get a random y value that fits within the hypnosis view's height
+        int height = (int)(self.view.bounds.size.height - messageLabel.bounds.size.height);
+        int y = arc4random() % height;
+        
+        // Update the label's frame
+        CGRect frame = messageLabel.frame;
+        frame.origin = CGPointMake(x, y);
+        messageLabel.frame = frame;
+        
+        //Add label to hierarchy
+        [self.view addSubview:messageLabel];
+        }
+}
 @end
